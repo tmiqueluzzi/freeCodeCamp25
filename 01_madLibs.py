3353fd2138c08542ae34e9930a207f0ec41706a2
Theo G. Miqueluzzi <90651324+tmiqueluzzi@users.noreply.github.com>
2021-09-14 18:01:18 -0300

    Add files via upload
    Fiz apenas um trecho. Dá pra colocar mais autores e fazer com que o usuário escolha.

---
+++ 01_madLibs.py
@@ -0,0 +1,20 @@
+#Apresentação
+print('Abaixo um trecho (editado com a sua ajuda, do livro "Mais esperto que o Diabo", de Napoleon Hill.')
+
+#variaveis
+substantivo_masc = input('Digite um substantivo masculino: ')
+adjetivo = input('Digite um adjetivo: ')
+adjetivo2 = input ('Digite outro adjetivo: ')
+substantivo_fem = input('Digite um substantivo feminino: ')
+adj_adv_modo = input('Digite um adjunto adverbial de modo: ')
+substantivo_fem2 = input('Digite um substantivo feminino: ')
+substantivo_masc2= input('Digite um substantivo masculino: ')
+verbo = input('Digite um verbo: ')
+verbo2 = input('Digite outro verbo: ')
+
+#resultado
+print(f'A causa para o {substantivo_masc} não é algo {adjetivo} e {adjetivo2} do homem, mas uma {substantivo_fem} '
+      f'que pode muito bem ser chamada de ‘Outro Eu’. Este ‘outro eu’ {adj_adv_modo} exerce sua {substantivo_fem2} '
+      f'ou se faz conhecer, exceto em {substantivo_masc2} de emergência, quando os homens são forçados, por '
+      f'meio das adversidades e das derrotas temporárias, a {verbo} seus hábitos e {verbo2} estratégias para '
+      f'sair das dificuldades. ')
